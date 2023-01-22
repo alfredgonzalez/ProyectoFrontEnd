6e0a8b184d2d1742ee64280f60500e99aae3fef6
AlfredoGonzalez <alfredoundav@gmail.com>
2023-01-22 19:21:14 -0300

    LuEmi2403

--- src/componentes/carousel/Carousel.js
+++ src/componentes/carousel/Carousel.js
@@ -5,7 +5,6 @@ import foto1 from "../img/foto1.png";
 import foto2 from "../img/foto2.png";
 import foto3 from "../img/foto3.png";
 import foto4 from "../img/foto4.jpg";
-<<<<<<< HEAD
 import foto5 from "../img/foto5.jpg";
 import foto6 from "../img/foto6.jpg";
 import foto7 from "../img/foto7.jpg";
@@ -19,8 +18,6 @@ import foto14 from "../img/foto14.jpg";
 import foto15 from "../img/foto15.jpg";
 
 
-=======
->>>>>>> 6acaab47dbd7552f5ebb8b747253ce1a28890179
 
 function CarouselFadeExample() {
   return (
